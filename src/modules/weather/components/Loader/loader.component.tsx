782e1a897259e16853a0ff062601170e9553ab8c
Merouane Rouibah <60047469+devMerouane@users.noreply.github.com>
2022-09-19 14:15:57 +0200

    add loader component

---
+++ src/modules/weather/components/Loader/loader.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './loader.module.css';
+
+const Loader = () => (
+  <div className={styles.container}>
+    <h2 className={styles.loader}>Loading ...</h2>
+  </div>
+);
+
+export default Loader;
