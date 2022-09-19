8ee77e100b6db00b1f4edbda4bb5c16b01f432b0
Merouane Rouibah <60047469+devMerouane@users.noreply.github.com>
2022-09-19 14:17:10 +0200

    export all components

---
+++ src/modules/weather/components/index.ts
@@ -0,0 +1,7 @@
+import Search from './Search';
+import Button from './Button';
+import Card from './Card';
+import Message from './Message';
+import Loader from './Loader';
+
+export { Search, Button, Card, Message, Loader };
